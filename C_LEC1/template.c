5ae62144cdf889e41fabfcfa94bf4fa659659fa5
HenryLee <gbvrcx@gmail.com>
2018-06-09 12:35:20 +0900

    add comments for template

--- C_LEC1/template.c
+++ C_LEC1/template.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+/* parsing arguments */
+/* DO NOT modify */
 struct __option {
     char file[80];
 };
@@ -11,7 +13,8 @@ void parse_option (option_t * opt, int argc, char** argv) {
         printf ("Usage: main <testfile>\n");
     }
     strcpy (opt->file, argv[1]);
-} 
+}
+/* DO NOT modify */
 
 int main (int argc, char** argv) {
     option_t opt;
@@ -19,6 +22,11 @@ int main (int argc, char** argv) {
 
     // TODO
     // 입력 파일의 이름은 opt.file에 저장되어 있다고 생각하면 됩니다.
+    // scanf 등을 이용하여 코딩해서 확인하고, 제출 할 때는 이 템플릿을 적용하여 제출 하면 됩니다.
+    
+    // 윈도우에서만 쓸 수 있는 _s 함수를 사용하지 않습니다.
+    // 맨 윗줄에 아래의 선언을 추가하면 됩니다.
+    // #define _CRT_SECCURE_NO_WARNINGS
 
     return 0;
 }
